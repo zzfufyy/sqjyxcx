52385efc91d3e904725cbb62087649edf0846035
zyw <278496680@qq.com>
2022-04-06 10:20:50 +0800

    bug 游览量

--- sqjyxcx/utils/url_util.js
+++ sqjyxcx/utils/url_util.js
@@ -1,8 +1,11 @@
 // 处理url映射问题
 
-
 const isImageUrlInServer = function (url) {
-    return url.substr(0, 1) == '/' ? true : false
+    if (typeof (url) === "undefined" || url === null || url.length == 0) {
+        return false;
+    }else{
+        return url.substr(0, 1) == '/' ? true : false
+    }
 }
 
 module.exports = {
